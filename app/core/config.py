14d70f367e1b87db78d4806e00635a73bf88029e
Mohammadreza-Farkhondeh <mohammr.farkhonde12@sharif.edu>
2025-01-23 17:53:00 +0330

    feat: update settings configuration to use SettingsConfigDict for improved environment handling

--- app/core/config.py
+++ app/core/config.py
@@ -1,4 +1,4 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
@@ -9,9 +9,9 @@ class Settings(BaseSettings):
     JWT_ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
 
-    class Config:
-        env_file = ".env"
-        extra = "allow"
+    model_config = SettingsConfigDict(
+        env_file=".env", env_file_encoding="utf-8", extra="allow"
+    )
 
 
 settings = Settings()
