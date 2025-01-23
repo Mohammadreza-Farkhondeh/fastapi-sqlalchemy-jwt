6e0177d4a5bf2d005a2bff63194b18a7c6f85d22
Mohammadreza-Farkhondeh <mohammr.farkhonde12@sharif.edu>
2025-01-23 14:41:00 +0330

    feat: update Alembic configuration and integrate database models for migrations

---
+++ app/db/__init__.py
@@ -0,0 +1,7 @@
+from app.db.session import Base
+
+# Import all models here (to be defined later)
+# from app.models.user import User
+
+
+__all__ = [Base]
--- app/db/base.py
+++
@@ -1,7 +0,0 @@
-from app.db.session import Base
-
-# Import all models here (to be defined later)
-# from app.models.user import User
-
-
-__all__ = [Base]
