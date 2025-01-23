f88a8307bb963ed3a5d89ae0d3aa816e11829652
Mohammadreza-Farkhondeh <mohammr.farkhonde12@sharif.edu>
2025-01-23 17:53:53 +0330

    feat: add User model with fields for username, email, hashed_password, and user roles

---
+++ app/models/user.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Boolean, Column, String
+
+from app.models.base import Base
+
+
+class User(Base):
+    username = Column(String, unique=True, nullable=False, index=True)
+    email = Column(String, unique=True, nullable=False, index=True)
+    hashed_password = Column(String, nullable=False)
+    is_active = Column(Boolean, default=True)
+    is_superuser = Column(Boolean, default=False)
