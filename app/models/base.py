c90aec6f04f0d35e8ae3481648a73b415ceebb68
Mohammadreza-Farkhondeh <mohammr.farkhonde12@sharif.edu>
2025-01-23 17:53:14 +0330

    refactor: update import statement for SQLAlchemy and streamline Base model definition

--- app/models/base.py
+++ app/models/base.py
@@ -1,16 +1,15 @@
 from datetime import datetime
 
 from sqlalchemy import Column, DateTime, Integer
-from sqlalchemy.ext.declarative import as_declarative, declared_attr
+from sqlalchemy.orm import as_declarative, declared_attr
 
 
 @as_declarative()
 class Base:
-    id: int
     __name__: str
 
     @declared_attr
-    def __tablename__(cls) -> str:
+    def __tablename__(cls):
         return cls.__name__.lower()
 
     id = Column(Integer, primary_key=True, index=True)
